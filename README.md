f3aa49e6e7f679514cd8956a8bc4085dcdfa8761
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2023-04-18 19:42:03 -0400

    ⭐️ init commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Foundry DAO Governance
+
+This is a section of the Cyfrin Foundry Solidity Course.
+
+*Please note: ERC20 based voting is not always recommended, and I encourage you to explore other forms of governance like reputation based or "skin-in-the-game" based.*
+
+[One of my favorite articles on money-based voting being bad](https://vitalik.ca/general/2018/03/28/plutocracy.html)
+
+- [Foundry DAO Governance](#foundry-dao-governance)
+- [Getting Started](#getting-started)
+  - [Requirements](#requirements)
+  - [Quickstart](#quickstart)
+    - [Optional Gitpod](#optional-gitpod)
+- [Usage](#usage)
+  - [Test](#test)
+  - [Deploy](#deploy)
+  - [Estimate gas](#estimate-gas)
+- [Formatting](#formatting)
+- [Thank you!](#thank-you)
+
+# Getting Started
+
+## Requirements
+
+- [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+  - You'll know you did it right if you can run `git --version` and you see a response like `git version x.x.x`
+- [foundry](https://getfoundry.sh/)
+  - You'll know you did it right if you can run `forge --version` and you see a response like `forge 0.2.0 (816e00b 2023-03-16T00:05:26.396218Z)`
+
+
+## Quickstart
+
+```
+git clone https://github.com/PatrickAlphaC/foundry-dao-f23
+cd foundry-dao-f23
+forge install
+forge build
+```
+
+### Optional Gitpod
+
+If you can't or don't want to run and install locally, you can work with this repo in Gitpod. If you do this, you can skip the `clone this repo` part.
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#github.com/PatrickAlphaC/foundry-dao-f23)
+
+# Usage
+
+## Test
+
+```
+forge test
+```
+## Deploy
+
+I did not write deploy scripts for this project, you can if you'd like!
+
+## Estimate gas
+
+You can estimate how much gas things cost by running:
+
+```
+forge snapshot
+```
+
+And you'll see and output file called `.gas-snapshot`
+
+
+# Formatting
+
+
+To run code formatting:
+```
+forge fmt
+```
+
+
+# Thank you!
+
+If you appreciated this, feel free to follow me or donate!
+
+ETH/Polygon/Avalanche/etc Address: 0x9680201d9c93d65a3603d2088d125e955c73BD65
+
+[![Patrick Collins Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/PatrickAlphaC)
+[![Patrick Collins YouTube](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/channel/UCn-3f8tw_E1jZvhuHatROwA)
+[![Patrick Collins Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/patrickalphac/)
+[![Patrick Collins Medium](https://img.shields.io/badge/Medium-000000?style=for-the-badge&logo=medium&logoColor=white)](https://medium.com/@patrick.collins_58673/)
---
+++ test/MyGovernorTest.t.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {GovToken} from "../src/GovToken.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+import {Box} from "../src/Box.sol";
+import {console} from "forge-std/console.sol";
+
+contract MyGovernorTest is Test {
+    GovToken token;
+    TimeLock timelock;
+    MyGovernor governor;
+    Box box;
+
+    uint256 public constant MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
+    uint256 public constant QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+    uint256 public constant VOTING_PERIOD = 50400; // This is how long voting lasts
+    uint256 public constant VOTING_DELAY = 1; // How many blocks till a proposal vote becomes active
+
+    address[] proposers;
+    address[] executors;
+
+    bytes[] functionCalls;
+    address[] addressesToCall;
+    uint256[] values;
+
+    address public constant VOTER = address(1);
+
+    function setUp() public {
+        token = new GovToken();
+        token.mint(VOTER, 100e18);
+        vm.prank(VOTER);
+        token.delegate(VOTER);
+        timelock = new TimeLock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(token, timelock);
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.TIMELOCK_ADMIN_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(0));
+        timelock.revokeRole(adminRole, msg.sender);
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(1);
+    }
+
+    function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 777;
+        string memory description = "Store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature(
+            "store(uint256)",
+            valueToStore
+        );
+        addressesToCall.push(address(box));
+        values.push(0);
+        functionCalls.push(encodedFunctionCall);
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(
+            addressesToCall,
+            values,
+            functionCalls,
+            description
+        );
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+        // governor.proposalSnapshot(proposalId)
+        // governor.proposalDeadline(proposalId)
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        // 2. Vote
+        string memory reason = "I like a do da cha cha";
+        // 0 = Against, 1 = For, 2 = Abstain for this example
+        uint8 voteWay = 1;
+        vm.prank(VOTER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        console.log("Proposal State:", uint256(governor.state(proposalId)));
+
+        // 3. Queue
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(addressesToCall, values, functionCalls, descriptionHash);
+        vm.roll(block.number + MIN_DELAY + 1);
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+
+        // 4. Execute
+        governor.execute(
+            addressesToCall,
+            values,
+            functionCalls,
+            descriptionHash
+        );
+
+        assert(box.retrieve() == valueToStore);
+    }
+}
