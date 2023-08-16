7e631bc94930e2c59890212186103b57df199911
ziruzavar <ivan.vanir@gmail.com>
2023-08-16 21:51:16 +0300

    Added name imports

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/governance/TimelockController.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
     // minDelay is how long you have to wait before executing
